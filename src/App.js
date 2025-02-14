228a0061075ee0e3edf5e6b742bd7289cba9f0ef
AndreasHadjisavvas99 <andis1999hass@outlook.com>
2025-02-14 22:33:56 +0200

    modified README file. add files package.json, package-lock.json and config-overrides.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FileUploadAndDisplay from './FileUploadAndDisplay'; // Make sure this path is correct
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <FileUploadAndDisplay />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
